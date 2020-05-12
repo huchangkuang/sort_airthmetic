0e5ed96ba2fdca8b2ddd1de1454eb51d9e52c8a2
huchangkuang <603021137@qq.com>
2020-05-12 16:53:10 +0800

    添加了猴子排序

---
+++ monkeySort.js
@@ -0,0 +1,29 @@
+function isOrder(arr) {
+    let is = true
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            is = false
+        }
+    }
+    return is
+}
+function randomSort(arr) {
+    let temp, x
+    for (let i = 0; i < arr.length; i++) {
+        x = Math.floor(Math.random() * (arr.length - 1))
+        temp = arr[i]
+        arr[i] = arr[x]
+        arr[x] = temp
+    }
+    return arr
+}
+function monkeySort(arr) {
+    let i = 0
+    while (!isOrder(arr)) {
+        randomSort(arr)
+        i++
+        console.log(`第${i}次：` + arr)
+    }
+    return arr
+}
+monkeySort([1, 5, 3, 6])
